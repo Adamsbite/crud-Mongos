97a0f74effea961bde7ddef4cc382c6b67d07b54
bright adams <btadamz@gmail.com>
2024-07-19 11:24:08 +0100

    lost

--- src/heros/heros.service.ts
+++ src/heros/heros.service.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import Hero from './schemas/hero.schemas';
 
 @Injectable()
-export class HerosService {
+export class  HerosService {
   constructor(
     @InjectModel(Hero.name) private heroModel: mongoose.Model<Hero>,
   ) {}
@@ -16,21 +16,21 @@ export class HerosService {
   async getHero(id) {
     const hero = await this.heroModel.findById(id);
     return hero;
-  }
-  async createHero(heroDetails) { 
-    console.log(heroDetails); 
+  } 
+  async createHero(heroDetails) {
+    console.log(heroDetails);
 
-    const hero =  await this.heroModel.create(heroDetails);
+    const hero = await this.heroModel.create(heroDetails);
     return hero;
   }
 
   async editHero(id, hero) {
     const editedHero = await this.heroModel.findByIdAndUpdate(id, hero, {
       new: true,
-    }); 
+    });
     return editedHero;
   }
-  async deleteHero(id) { 
+  async deleteHero(id) {
     const hero = await this.heroModel.findByIdAndDelete(id);
     return hero;
   }
